4fb717fb41ef0455b064f072541e0c6a4848cb2e
vkpvivek <vkpvivek67@gmail.com>
2023-11-11 08:38:33 +0530

    Object, array and Generic type

--- New Test/app.js
+++ New Test/app.js
@@ -10,11 +10,30 @@ function Add(num1, num2) {
     }
     return +num1 + +num2;
 }
+//Object & Arrays:
+var numResult = []; //genereic type
+var textResult = [];
+function printResult(resObj) {
+    console.log(resObj);
+}
+;
+//function for Add
 buttonElem.addEventListener('click', function () {
     var num1 = num1Elem.value;
     var num2 = num2Elem.value;
     var result = Add(+num1, +num2);
     var stringResult = Add(num1, num2);
+    numResult.push(result);
+    textResult.push(stringResult);
     console.log(result);
     console.log(stringResult);
+    console.log(numResult, textResult);
+});
+var myPromise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve('It worked');
+    }, 1000);
+});
+myPromise.then(function (res) {
+    console.log(res.split('w'));
 });
--- New Test/app.ts
+++ New Test/app.ts
@@ -2,7 +2,8 @@ const num1Elem=document.getElementById('num1') as HTMLInputElement;
 const num2Elem=document.getElementById('num2') as HTMLInputElement;
 const buttonElem =document.querySelector('button')!;
 
-function Add(num1:number|String, num2:number|String) {
+
+function Add(num1:NumOrString, num2:NumOrString) {
     if(typeof num1==='number' && typeof num2==='number'){
         return num1 + num2;
     }
@@ -12,6 +13,22 @@ function Add(num1:number|String, num2:number|String) {
     return +num1 + +num2;
 }
 
+//Type Aliases 
+//[instead of write same type/object multiple place define once and use at other places ]
+type NumOrString= number|String;
+type Result= {val:number; timestamp:Date};
+
+//Object & Arrays:
+const numResult:Array<number>=[];  //genereic type
+const textResult:string[]=[];
+
+function printResult(resObj:Result) {
+    console.log(resObj);
+};
+
+
+
+//function for Add
 buttonElem.addEventListener('click',()=>{
     const num1=num1Elem.value;
     const num2=num2Elem.value;
@@ -19,8 +36,21 @@ buttonElem.addEventListener('click',()=>{
     const result= Add(+num1,+num2);
     const stringResult=Add(num1,num2);
 
+    numResult.push(result as number);
+    textResult.push(stringResult as string);
+
     console.log(result);
     console.log(stringResult);
+    console.log(numResult,textResult);
 });
 
 
+const myPromise=new Promise<string>((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve('It worked');
+    },1000);
+});
+
+myPromise.then(res=>{
+    console.log(res.split('w'));
+})
